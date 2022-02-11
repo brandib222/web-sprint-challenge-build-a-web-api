6598c187f41e62a5ae2bd47dc73bf4598486f30a
brandib222 <brandi.danielle.ball@gmail.com>
2022-02-11 11:16:46 -0500

    omg i dont need to commit but i got httpie working and im very excited

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`<h2>OMG I HOPE THIS WORKS</h2>`)
+})
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
